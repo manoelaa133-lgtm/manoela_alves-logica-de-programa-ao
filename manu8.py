f524393e953c08ab7755d8246df20fd5679157ea
manoelaa133-lgtm <manoelaa133@gmail.com>
2026-06-05 23:04:20 -0300

    questão 9
    cadastro de alunos

---
+++ manu8.py
@@ -0,0 +1,9 @@
+aluno = {
+    "nome": "Manoela",
+    "idade": 18,
+    "curso": "engenharia civil"
+}
+
+print("nome:", aluno["nome"])
+print("idade:", aluno["idade"])
+print("curso:", aluno ["curso"])
